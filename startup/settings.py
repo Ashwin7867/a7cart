a7e1e4e1fef14c719c9aafb34ecd23b758c22464
Ashwin Ghagre <36293193+Ashwin7867@users.noreply.github.com>
2020-04-30 12:14:09 +0530

    Update settings.py

--- startup/settings.py
+++ startup/settings.py
@@ -181,9 +181,16 @@ WSGI_APPLICATION = 'startup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-
-DATABASES['default'] = dj_database_url.config(conn_max_age = 600, ssl_require =True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST':'',
+        'PORT':'',
+    }
+}
 
 HAYSTACK_CONNECTIONS = {
     'default': {
