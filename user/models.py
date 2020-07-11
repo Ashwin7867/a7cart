d1f324a0b462a1984b3a2da9caa8eaf3a3b03619
Ashwin Ghagre <36293193+Ashwin7867@users.noreply.github.com>
2020-07-11 22:44:51 +0530

    Update models.py

--- user/models.py
+++ user/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from oscar.apps.customer.abstract_models import AbstractUser
 
 class User(AbstractUser):
-    username = models.CharField(max_length = 10, unique = True)
-    first_name = models.CharField(max_length = 15)
-    last_name = models.CharField(max_length = 15)
+    username = models.CharField(max_length = 30, unique = True)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
     phone = models.CharField(max_length = 13, unique = True)
     email = models.EmailField(unique = True, blank = True)
-    password = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 100)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = [ 'first_name', 'last_name','phone','email', 'password']
