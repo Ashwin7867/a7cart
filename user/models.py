8c422c2f4765551253f41e425cf08b4ab427b044
Ashwin7867 <ashwin89872@gmail.com>
2020-07-11 23:59:59 +0530

    ashwin_

--- user/models.py
+++ user/models.py
@@ -8,11 +8,7 @@ class User(AbstractUser):
     last_name = models.CharField(max_length = 30)
     phone = models.CharField(max_length = 13, unique = True)
     email = models.EmailField(unique = True, blank = True)
-<<<<<<< HEAD
     password = models.CharField(max_length = 200)
-=======
-    password = models.CharField(max_length = 150)
->>>>>>> f95c4ecc652e5a7763b02ba4e54cc1e231cccbb4
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = [ 'first_name', 'last_name','phone','email', 'password']
