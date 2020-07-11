3b0dc10317af3516008be514af16afa5b908b617
Ashwin7867 <ashwin89872@gmail.com>
2020-07-11 23:39:00 +0530

    final_co

--- user/models.py
+++ user/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from oscar.apps.customer.abstract_models import AbstractUser
 
 class User(AbstractUser):
-    username = models.CharField(max_length = 10, unique = True)
-    first_name = models.CharField(max_length = 15)
-    last_name = models.CharField(max_length = 15)
+    username = models.CharField(max_length = 30, unique = True)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
     phone = models.CharField(max_length = 13, unique = True)
     email = models.EmailField(unique = True, blank = True)
     password = models.CharField(max_length = 200)
